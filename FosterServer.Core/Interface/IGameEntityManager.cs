bd64516a8f83738c5db4826bda343e5e2ce9d2ba
jeremy <jeremyfoster07@yahoo.com>
2022-09-25 17:03:10 -0500

    Game Entity Models updated to track additional properties Interface added for GameEntity

---
+++ FosterServer.Core/Interface/IGameEntityManager.cs
@@ -0,0 +1,25 @@
+﻿using FosterServer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FosterServer.Core.Interface
+{
+    public interface IGameEntityManager
+    {
+        void Initialize(float a_X, float a_Y, Size a_entitySize, float a_rotation = 0, bool a_isInteractable = false, bool a_canEntityPassThrough = false);
+
+        void ToggleCanEntityBePassedThrough();
+
+        void SetPosition(Vector3 a_position);
+
+        void SetPosition(float a_x, float a_y, float a_z = 1);
+
+        void SetRotation(float a_rotate);
+
+        bool HasGridPointIntersect(GridPoint a_sourcePoint);
+    }
+}
