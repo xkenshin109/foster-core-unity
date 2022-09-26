d4ced73389cc0e1b92750ad19dfa4e77db347c92
jeremy <jeremyfoster07@yahoo.com>
2022-09-25 19:47:20 -0500

    Dice Manager added to handle dice rolls

---
+++ FosterServer.Core/Enumerations/Dices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FosterServer.Core.Enumerations
+{
+    public enum Dices
+    {
+        [Display(Name = "4-Sided Dice")]
+        D4 = 4,
+        [Display(Name = "6-Sided Dice")]
+        D6 = 6,
+        [Display(Name = "8-Sided Dice")]
+        D8 = 8,
+        [Display(Name = "10-Sided Dice Percentage")]
+        D10P = 10,
+        [Display(Name = "10-Sided Dice")]
+        D10 = 10,
+        [Display(Name = "12-Sided Dice")]
+        D12 = 10,
+        [Display(Name = "20-Sided Dice")]
+        D20 = 20
+    }
+}
---
+++ FosterServer.Core/Manager/DiceManager.cs
@@ -0,0 +1,30 @@
+﻿using FosterServer.Core.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace FosterServer.Core.Manager
+{
+    public static class DiceManager
+    {
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+        public static float RollDice(Dices a_dice)
+        {
+            byte[] randomNumber = new byte[1];
+            _generator.GetBytes(randomNumber);
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+            // We are using Math.Max, and substracting 0.00000000001, 
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = (int)a_dice;
+            double randomValueInRange = Math.Floor(multiplier * range) + 1;
+            return (float)randomValueInRange;
+        }
+
+    }
+}
